172b33e7b9f43605bd96965751efbd0907027b3a
“gerald <fitzgeraldkachi@gmail.com>
2024-01-25 14:42:55 +0100

    latest

--- src/components/Feedbacks.tsx
+++ src/components/Feedbacks.tsx
@@ -61,7 +61,7 @@ const Feedbacks = () => {
           <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
       </div>
-      <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap items-center justify-center gap-7`}>
+      <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap sm:flex-nowrap items-center justify-center gap-7`}>
         {testimonials.map((testimonial, index) => (
           <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
         ))}
